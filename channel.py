67e7d98d412a1301c205e9db33376f5aa0d77706
Josh Kunz <thejoshkunz@gmail.com>
2011-08-25 15:10:20 -0700

    Added PyChannel to source control

---
+++ channel.py
@@ -0,0 +1,499 @@
+from flask import Flask, request, abort, g, render_template, redirect, url_for, send_from_directory, session, flash
+import redis
+import time
+import ImageFile, Image
+import os
+import ConfigParser
+import cPickle as cP
+import bcrypt
+import functools
+import hashlib
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = '\x02;\xa0o!\x0f\x14W\x97\x87\xeb\xab\xa4\xa1:\xca\xb7\x8dj9T\x97\x1e\x88'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif', 'jpeg'])
+
+##Check to see if the required stuff is here
+
+def require(cls, *rargs):
+	"A Decorator to allow a command to require certain meta information..."
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs):
+			for a in rargs:
+				if a not in kwargs: return None
+			return func(*args, **kwargs)
+		return wrapper
+	return decorator
+
+class Tripcode(object):
+	
+	@classmethod
+	def get(self, username):
+		return cP.loads(g.r.get("trip:{0}".format(username)))
+		
+	def __new__(cls, username, passw="", **kwargs):
+		"Return the Tripcode instance from redis if the username is already registered"
+		if not kwargs.get("skip_check") and g.r.exists("trip:{0}".format(username)):
+			return Tripcode.get(username)
+		else:
+			return super(Tripcode, cls).__new__(cls, username, passw)
+	
+	def __init__(self, username, trip="", **kwargs):
+		
+		self.passwd = bcrypt.hashpw(trip, bcrypt.gensalt(5))
+		self.username = username
+		
+	def get_level(self):
+		return self.__dict__.get("level")
+		
+	def set_permission(self, permission_level):
+		self.level = permission_level
+		
+	def set_pass(self, password):
+		self.passwd = bcrypt.hashpw(password, bcrypt.gensalt())
+		
+	def save(self):
+		g.r.set("trip:{0}".format(self.username), cP.dumps(self))
+
+class Commands(object):
+	
+	@classmethod
+	def mapped_method(cls, method_name):
+		if hasattr(cls, method_name):
+			return getattr(cls, method_name)
+	
+	@classmethod
+	@require("trip", "author_line")
+	def login(cls, trip=None, author_line="", **kwargs):
+		print "l", trip.get_level()
+		print "p", trip.passwd
+		if trip.get_level() in ["admin", "mod"] and \
+		bcrypt.hashpw(author_line.rsplit("#", 1)[1], trip.passwd) == trip.passwd:
+			session["level"] = trip.get_level()
+			session["user"] = trip.username
+		return redirect(request.environ["HTTP_REFERER"])
+		
+	@classmethod
+	def logout(cls, **kwargs):
+		if session.get("level"):
+			map(session.pop, ("level", "user"))
+		return redirect(request.environ["HTTP_REFERER"])
+		
+	@classmethod
+	@require("trip")
+	def regi(cls, trip=None, level="mod", **kwargs):
+		trip.set_permission(self, level)
+		trip.save()
+		return redirect(request.environ["HTTP_REFERER"])
+		
+	@classmethod
+	@require("trip", "author_line")
+	def cpass(cls, trip=None, author_line="", **kwargs):
+		trip.set_password(author_line.rsplit("#", 1)[1])
+		trip.save()
+		
+	@classmethod
+	@require("post")
+	def asa(cls, post=None, **kwargs):
+		if session.get("level"):
+			post.capcode = "## {0} ##".format(session.get("level").capitalize())
+		
+class PostImage(object):
+	
+	@classmethod
+	def hash(cls, image_stream):
+		h = hashlib.sha1()
+		image_stream.stream.seek(0, 0) #seek to the begining of the file
+		while True: #read the file
+			b = image_stream.stream.read(8192)
+			if not b: break
+			h.update(b)
+		return h.hexdigest()
+	
+	def __new__(cls, image=None):
+		print "im new, image is:", image
+		if image:
+			hash = cls.hash(image)
+			if g.r.exists("image:{0}".format(hash)):
+				print "Image Passed but exists..."
+				return cP.loads(g.r.get("image:{0}".format(hash)))
+			print "Image passed, but does not exist..."
+			return super(PostImage, cls).__new__(cls, image, False)
+		print "No image Passed"
+		return super(PostImage, cls).__new__(cls, image, True)
+	
+	def __init__(self, image, exists):
+		print "in init"
+		print image
+		if image and not exists:
+			self.hash = PostImage.hash(image)
+			self.__stream  = image.stream
+			self.filename = image.filename
+			self.id = int(time.time())
+		print "INIT: Exists?", exists
+		self.__exists = exists
+		
+	def save(self, thumbnail_size=(250, 250)):
+		"Saves an image if the image does not already exist..."
+		if not self.__exists: self.__save_routine(thumbnail_size)
+		
+	def __save_path(self):
+		"Returns the path to save the images to"
+		return g.conf.get("site", "image_store")
+		
+	def __save_redis(self):
+		delattr(self, "_PostImage__stream")
+		g.r.set("image:{0}".format(self.hash), cP.dumps(self))
+		
+	def __save_routine(self, thumbnail_size=(250, 250)):
+		extension = self.filename.rsplit('.', 1)[1]
+		if '.' in self.filename and extension in ALLOWED_EXTENSIONS:
+			
+			self.__stream.seek(0, 0) #seek to the begining
+			
+			#Read the image stream into a PIL Image class
+			image_p = ImageFile.Parser()
+			image_p.feed(self.__stream.read(-1))
+			im = image_p.close()
+			
+			#save the image
+			im.save(os.path.join(self.__save_path(), str(self.id)+"."+extension))
+			
+			self.resolution = im.size
+			self.format = im.format
+			
+			#thumbnail the image
+			im.thumbnail(thumbnail_size, Image.ANTIALIAS)
+			im.save(os.path.join(self.__save_path(), ".".join(["thumb", str(self.id), extension])))
+			self.__stream.seek(0,2)
+			
+			self.filesize = self.__stream.tell()
+			self.url = str(self.id)+"."+extension
+			self.thumb_url = ".".join(["thumb", str(self.id), extension])
+			
+			self.__save_redis()
+	
+class Board(object):
+	
+	def __init__(self, board):
+		self.short = board
+		self.title = g.conf.get("boards", self.short)
+	
+	def threads(self, start_index=0, stop_index=-1):
+		return Thread.threads_on_board(self.short, start_index, stop_index)
+
+class Thread(object):
+	"This is a thread"
+	
+	@classmethod
+	def threads_on_board(cls, board, start_index=0, stop_index=-1):
+		return [cP.loads(g.r.get("thread:{0}".format(post_id))) for post_id in g.r.zrevrange("board:{0}:threads".format(board), start_index, stop_index) ]
+		
+	@classmethod
+	def bump_thread(cls, board, thread_id):
+		g.r.zadd("board:{0}:threads".format(board), thread_id, int(time.time())) #These are swapped in the py-redis api and not to spec
+		
+	def replies(self, start_index=0, stop_index=-1):
+		return Reply.replies_to_thread(self.id, start_index, stop_index)
+		
+	def __init__(self,**kwargs):
+		self.id = int(time.time())
+		self.__dict__.update(kwargs)
+		
+	def save(self):
+		g.r.zadd("board:{0}:threads".format(self.board), self.id, self.id) #These are swapped in the py-redis api and not to spec
+		g.r.set("thread:{0}".format(self.id), cP.dumps(self, protocol=-1))
+		
+class Reply(object):
+	"This is a reply"
+	
+	@classmethod
+	def from_id(cls, reply_id):
+		return cP.loads(g.r.get("reply:{0}".format(reply_id)))
+		
+	@classmethod
+	def replies_to_thread(cls, thread_id, start_index=0, stop_index=-1):
+		return [cP.loads(g.r.get("reply:{0}".format(reply_id))) for reply_id in g.r.zrange("thread:{0}:replies".format(thread_id), start_index, stop_index) ]
+		
+	def __init__(self, thread_id, **kwargs):
+		t_now = int(time.time())
+		self.id = t_now
+		self.thread = thread_id
+		self.__dict__.update(kwargs)
+		
+	def save(self, bump_thread=True):
+		g.r.zadd("thread:{0}:replies".format(self.thread), self.id, self.id)
+		g.r.set("reply:{0}".format(self.id), cP.dumps(self, protocol=-1))
+		if bump_thread: Thread.bump_thread(self.board, self.thread)
+		
+def check_redis():
+	"Check to see if we can connect to redis"
+	r_server = redis.Redis(db="2channel")
+	try:
+		r_server.ping()
+	except redis.ConnectionError, e:
+		print "## ERROR: The redis server appears not to be running."
+		raise e
+	r_server = None #add the redis stuff to the garbage
+
+def handle_image(image, id):
+	"Check to see if imags is allowed, and save to disk."
+	extension = image.filename.rsplit('.', 1)[1]
+	if '.' in image.filename and extension in ALLOWED_EXTENSIONS:
+		image_dict = {}
+		#image.save(os.path.join(g.conf.get("site", "image_store"), str(t_now)+"."+extension))
+		image_p = ImageFile.Parser()
+		image_p.feed(image.stream.read(-1))
+		im = image_p.close()
+		im.save(os.path.join(g.conf.get("site", "image_store"), str(id)+"."+extension))
+		image_dict["resolution"] = im.size
+		image_dict["format"] = im.format
+		im.thumbnail((250, 250), Image.ANTIALIAS)
+		im.save(os.path.join(g.conf.get("site", "image_store"), ".".join(["thumb", str(id), extension])))
+		image.stream.seek(0,2)
+		image_dict.update({
+			"name": image.filename,
+			"filesize": image.stream.tell(),
+			"url": str(id)+"."+extension,
+			"thumb_url": ".".join(["thumb", str(id), extension])
+		})
+		return image_dict
+	else:
+		return None
+
+def get_opt(section, option, default=None, type="str"):
+	"Get a config option. Auto checks for aproprite[sp] sections and options."
+	get_types = {
+		"str": g.conf.get,
+		"bool": g.conf.getboolean,
+		"int": g.conf.getint,
+		"float": g.conf.getfloat
+	}
+	if not g.conf.has_section(section):
+		return default
+	elif not g.conf.has_option(section, option):
+		return default
+	return get_types[type](section, option)
+	
+def get_post_opts(subject_line, author_line):
+	
+	meta = {
+		"subject_line": subject_line,
+		"author_line": author_line
+	}
+	
+	sl = subject_line.rsplit(get_opt("site", "command_sep", "#!"), 1)
+	meta["subject"] = sl[0]
+	if len(sl) > 1: meta["command"] = Commands.mapped_method(sl[1])
+	
+	al = author_line.rsplit("#", 1)
+	meta["author"] = al[0]
+	if len(al) > 1: meta["trip"] = Tripcode(meta["author"], al[1])
+	
+	return meta
+			
+@app.before_request
+def setup_globals():
+	"Setup the global options."
+	g.r = redis.Redis(db="2channel") #Ini Redis
+	g.conf = ConfigParser.SafeConfigParser() #Ini the configs
+	g.conf.readfp(open("channel.ini"))
+	
+@app.route('/')
+def index():
+	"Index of the site."
+	return render_template("index.html")
+	
+@app.errorhandler(400)
+def error(error):
+	"Show general error messages sent via flash()s"
+	return render_template("error.html", rd=request.base_url)
+	
+@app.route("/images/<image_url>/delete")
+def delete_image(image_url):
+	"Removes an image"
+	
+	if session.get("level"):
+		try:
+			os.remove(os.path.join(g.conf.get("site", "image_store"), image_url))
+			os.remove(os.path.join(g.conf.get("site", "image_store"), "thumb."+image_url))
+		except OSError:
+			flash("No image: {0}".format(image_url))
+			abort(400)
+		
+	return redirect(request.environ["HTTP_REFERER"])
+		
+@app.route("/boards/<board>/<int:thread_id>/delete")
+def delete_thread(board, thread_id):
+	"Remove a thread"
+	
+	if session.get("level") and g.r.exists("thread:{0}".format(thread_id)):
+		replies = ["reply:{0}".format(reply) for reply in g.r.lrange("thread:{0}:replies".format(thread_id), 0, -1)]
+		pipe = g.r.pipeline()
+		if replies:
+			pipe.delete(*replies)
+			pipe.delete("thread:{0}:replies".format(thread_id))
+		pipe.delete("thread:{0}".format(thread_id))
+		pipe.zrem("board:{0}:threads".format(board), thread_id)
+		pipe.execute()
+	
+	return redirect(request.environ["HTTP_REFERER"])
+	
+@app.route("/boards/<board>/<int:thread_id>/<int:reply_id>/delete")
+def delete_reply(board, thread_id, reply_id):
+	
+	if session.get("level") and g.r.exists("reply:{0}".format(reply_id)):
+		g.r.lrem("thread:{0}:replies".format(thread_id), reply_id)
+		g.r.delete("reply:{0}".format(reply_id))
+	
+	return redirect(request.environ["HTTP_REFERER"])
+
+@app.route('/images/<filename>')
+def uploaded_file(filename):
+	"Return an uploaded file."
+	return send_from_directory(g.conf.get("site", "image_store"), filename)
+
+@app.route('/boards/<board>', methods=['POST'])
+def post(board):
+	"Add a post to the board"
+	sec = "{0}:options".format(board)
+	if board not in g.conf.options("boards"):
+		abort(404)
+	elif not get_opt(sec, "allow_threads", True, "bool"):
+		flash("Thread creation not allowed.")
+		abort(400)
+		
+	meta = get_post_opts(request.form.get("subject", ""),
+						 request.form.get("author", "Anonymous"))
+	meta["post"] = Thread(text = request.form["content"],
+						  subject = meta["subject"],
+						  author = meta["author"],
+						  board = board)
+	if meta.get("command"):
+		r = meta["command"](**meta)
+		if r: return r
+	
+	if get_opt(sec, "require_thread_subject", False, "bool") and not meta["post"].subject:
+		flash("Thread subject required.")
+		abort(400)
+	
+	if get_opt(sec, "require_thread_author", False, "bool") and not meta["post"].author:
+		flash("Thread author name required.")
+		abort(400)
+		
+	image = request.files.get("image", None)
+	if get_opt(sec, "allow_thread_images", True, "bool") and image:
+		meta["post"].image = PostImage(image)
+		meta["post"].image.save()
+	elif not image and get_opt(sec, "require_thread_image", False, "bool"):
+		flash("Thread image required for posting.")
+		abort(400)
+	
+	print "p", meta["post"].image.__dict__
+	meta["post"].save()
+	
+	#if get_opt(sec, "prune_threads", True, "bool") \
+	#and g.r.zcard("board:{0}:threads".format(board)) > get_opt(sec, "prune_threads_after", 250, "int"):
+	#	to_prune = g.r.zrevrange("board:{0}:threads".format(board),
+	#							 get_opt(sec, "prune_threads_after", 250, "int"),
+	#							 -1)
+	#	for thread in to_prune:
+	#		replies = ["reply:{0}".format(reply) for reply in g.r.lrange("thread:{0}:replies".format(thread), 0, -1)]
+	#		pipe = g.r.pipeline()
+	#		if replies:
+	#			pipe.delete(*replies)
+	#			pipe.delete("thread:{0}:replies".format(thread))
+	#		pipe.delete("thread:{0}".format(thread))
+	#		pipe.zrem("board:{0}:threads".format(board), thread)
+	#		pipe.execute()
+	#		print thread, "pruned"
+			
+	
+	return redirect(url_for("board", board=board))
+
+@app.route('/boards/<board>')
+def board(board):
+	"Get a board's threads."
+	if board not in g.conf.options("boards"):
+		abort(404)
+	else:
+		return render_template("board.html", board = Board(board))
+		
+@app.route('/boards/<board>/<int:thread_id>', methods=['POST'])
+def reply(board, thread_id):
+	"Post a reply to a thread with thread_id"
+	sec = "{0}:options".format(board)
+	if board not in g.conf.options("boards") or not g.r.exists("thread:{0}".format(thread_id)):
+		abort(404)
+	elif not get_opt(sec, "allow_replies", True, "bool"):
+		flash("Replies not allowed.")
+		abort(400)
+		
+	meta = get_post_opts(request.form.get("subject", ""),
+						 request.form.get("author", "Anonymous"))
+	
+	r = m
+		
+	capcode = ""
+	if meta["command"].__name__ == "asa" and session.get("level"):
+		capcode = "## {0} ##".format(session.get("level").capitalize())
+		print capcode
+	
+	image = request.files.get("image", None)
+	reply = Reply(thread_id,
+				  text = request.form["content"],
+				  subject = subject,
+				  author = author,
+				  capcode = capcode,
+				  board = board)
+	
+	reply.save()
+	
+	#if get_opt(sec, "require_reply_subject", False, "bool") and not reply.subject:
+	#	flash("Reply subject required.")
+	#	abort(400)
+	#
+	#if get_opt(sec, "require_reply_author", False, "bool") and not reply.author:
+	#	flash("Reply author name required.")
+	#	abort(400)
+	#
+	##post the image
+	#if image and get_opt(sec, "allow_reply_images", True, "bool"):
+	#		if handle_ret: reply.image = handle_ret
+	#if image and not get_opt(sec, "allow_reply_images", True, "bool"):
+	#	flash("Images not allowed on this board")
+	#	abort(400)
+	#elif not image and get_opt(sec, "require_reply_image", False, "bool"):
+	#		flash("Reply requires and image.")
+	#		abort(400)
+	#
+	#g.r.rpush("thread:{0}:replies".format(thread_id), t_now)
+	#if get_opt("{0}:options".format(board), "enable_thread_bumping", True, "bool") and comm != "sage":
+	#	g.r.zadd("board:{0}:threads".format(board), thread_id, t_now) #These are swapped in the py-redis api and not to spec
+	#g.r.set("reply:{0}".format(t_now), cP.dumps(values, protocol=-1))
+	
+	return redirect(url_for("thread", board=board, thread_id=thread_id))
+	
+@app.route('/boards/<board>/<int:thread_id>')
+def thread(board, thread_id):
+	"Retrive a thread"
+	if board not in g.conf.options("boards") or not g.r.exists("thread:{0}".format(thread_id)):
+		abort(404)
+		
+	else:
+		return render_template("thread.html",
+							   board = Board(board),
+							   thread = cP.loads(g.r.get("thread:{0}".format(thread_id))))
+	
+
+if __name__ == '__main__':
+	"Run via flask runtime if not running as WSGI"
+	import sys
+	port = 80 if len(sys.argv) < 2 else int(sys.argv[1])
+	app.run(port = port)
+	
+elif __name__ != '__main__':
+	application  = app #so as to comply with wsgi standards
